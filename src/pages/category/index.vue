b8be677086099aa0f1fc018d679312f569eefe11
zhouyuCN <928017614@qq.com>
2019-04-14 16:18:41 +0800

    商品分类渲染

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -8,7 +8,7 @@
     <div class="content">
       <div class="left">
         <ul>
-          <li v-for="(item,index) in categories" :key="index" ><a href="#" @click="selectedIndex=index"  :class="{active:selectedIndex===index}">{{item.cat_name}}</a> </li>
+          <li v-for="(item,index) in categories" :key="index" ><a href="#" @click="setRight(index)"  :class="{active:selectedIndex===index}">{{item.cat_name}}</a> </li>
           
         </ul>
       </div>
@@ -16,94 +16,16 @@
         <div class="title-img">
           <img src="https://img.alicdn.com/tfs/TB1ZkoLukCWBuNjy0FaXXXUlXXa-966-644.jpg_490x490q100.jpg_.webp" alt="">
         </div>
-        <div class="category-list" >
+        <div class="category-list" v-for="(item,index) in rightData" :key="index" >
           <div class="category-list-title" >
-            <span class="icon">/</span><span class="title-name">电视</span><span class="icon">/</span>
+            <span class="icon">/</span><span class="title-name">{{item.cat_name}}</span><span class="icon">/</span>
           </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
+          <div class="category-item" v-for="(setitem,setindex) in item.children"  :key="setindex">
+            <img :src="'https://itjustfun.cn/'+setitem.cat_icon"  alt="">
+            <div>{{setitem.cat_name}}</div>
           </div>
 
-        </div>
-            <div class="category-list" >
-          <div class="category-list-title" >
-            <span class="icon">/</span><span class="title-name">电视</span><span class="icon">/</span>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-
-        </div>
-            <div class="category-list" >
-          <div class="category-list-title" >
-            <span class="icon">/</span><span class="title-name">电视</span><span class="icon">/</span>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-
-        </div>
-            <div class="category-list" >
-          <div class="category-list-title" >
-            <span class="icon">/</span><span class="title-name">电视</span><span class="icon">/</span>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-          <div class="category-item">
-            <img src="//mcdn.pinduoduo.com/assets/img/mpdd_fresh_fruit_2.jpg" alt="">
-            <div>曲面电视</div>
-          </div>
-
-        </div>
+        </div>  
       </div>
     </div>
   </div>
@@ -117,17 +39,26 @@ export default {
     return {
       categories:{},
       selectedIndex:0,
+      rightData:{}
      
+    }
+  },
+  methods: {
+    setRight(index){
+      this.selectedIndex=index;
+      this.rightData=this.categories[index].children;
+      console.log(this.rightData);
+      
+      
+      
+
     }
   },
  async mounted() {
     let res = await request('https://itjustfun.cn/api/public/v1/categories');
     console.log(res);
     this.categories=res.data.data;
-  
-   
-
-  
+     this.setRight(this.selectedIndex)
     
     
   },
