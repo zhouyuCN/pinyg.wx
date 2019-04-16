1fc8eea10de68893c8a478e61d04c5a0bf1fd292
zhouyuCN <928017614@qq.com>
2019-04-16 11:12:33 +0800

    完善上拉下拉

--- src/pages/searchlist/index.vue
+++ src/pages/searchlist/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="top">
+    <div class="top" :style="{position:isScroll}">
       <!-- 头部 -->
       <div class="header">
         <div class="search">
@@ -21,7 +21,7 @@
     </div>
 
     <!-- 内容 -->
-    <div class="bottom">
+    <div class="bottom" :style="{marginTop:margintop}">
       <div class="item" v-for="(item,index) in searchList" :key="index">
         <img :src="item.goods_big_logo" alt>
         <div class="content">
@@ -47,26 +47,25 @@ export default {
       query: "",
       pagenum: 1,
       pagesize: 10,
-      isEnd: false
+      isEnd: false,
+      isScroll:'static',// 设置元素默认为静态定位
+      margintop:"0rpx"
     };
   },
   methods: {
     async getData() {
       // 在导航条中添加加载动画
-      wx.showNavigationBarLoading();
       if (this.isEnd) {
-        // 提示用户数据已经全部加载
+        // 提示信息
         wx.showToast({
-          title: "数据加载完毕",
+          title: "数据已经加载完",
           icon: "none",
           duration: 2000
         });
+
         return;
       }
-      // 打开加载动画
-      wx.showLoading({
-        title: "加载完成"
-      });
+
       // 得到请求的列表数据
       this.query = this.$root.$mp.query.query;
       let res = await request.get(
@@ -78,17 +77,10 @@ export default {
         }
       );
       console.log(res);
-
+      // 当返回的数据的长度为 0 时就应该停止请求
       if (res.data.data.goods.length === 0) {
-        // 提示用户数据已经全部加载
-        wx.showToast({
-          title: "数据已经加载完毕",
-          icon: "none",
-          duration: 2000
-        });
         // 将开关设置为关
         this.isEnd = true;
-        return;
       }
       // 当第一次加载时，要直接赋值
       // 第二次及以后，不是直接赋值，而是将两个数组进行拼接 concat
@@ -97,12 +89,9 @@ export default {
       } else {
         this.searchList = this.searchList.concat(res.data.data.goods);
       }
-      // 关闭加载框
       wx.hideLoading();
       // 关闭下拉框
       wx.stopPullDownRefresh();
-      // 关闭导航中的加载框
-      wx.hideNavigationBarLoading();
     }
   },
 
@@ -114,17 +103,34 @@ export default {
     console.log(this.query);
     this.getData();
   },
+  onUnload() {
+    this.pagenum = 1;
+    this.query = "";
+    this.searchList = [];
+  },
   // 上拉触底事件
   onReachBottom() {
     this.pagenum = this.pagenum + 1;
     this.getData();
   },
+   onPageScroll(scroll) {
+    // 当事件被触发时将头部设置为固定定位，否则（没有固定定位）
+    // 操作 dom
+    if (scroll.scrollTop == 0) {
+      this.isScroll = 'static'
+      this.margintop = '0rpx'
+    } else {
+      this.isScroll = 'fixed'
+      this.margintop = '200rpx'
+    }
+  },
   // 下拉刷新
   onPullDownRefresh() {
     // 刷新页面（将所有数据恢复到默认值）
-    this.pagenum = 0;
+    this.pagenum = 1;
     this.searchList = [];
     this.isEnd = false;
+     this.isScroll = 'static';
     // 数据重新请求
     this.getData();
   }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,26 @@
-var request = function (url,method='get',data={},header={}) {
+var request = function (url, method = 'get', data = {}, header = {}) {
     return new Promise((resolve, reject) => {
+      
+          // 打开加载动画
+          wx.showLoading({
+            title: "努力加载中",
+            duration: 2000
+        });
         wx.request({
+
             url: url,
             method: method,
             data: data,
             header: header,
             success: function (res) {
+                // // 打开加载动画
+                // wx.showLoading({
+                //     title: "加载完成"
+                // });
+
                 resolve(res);
+                // 关闭加载框
+                wx.hideLoading();
             },
             fail: function (res) {
                 reject(res);
@@ -15,8 +29,8 @@ var request = function (url,method='get',data={},header={}) {
     });
 };
 
-request.get=function(url,data){
-    return request(url,'get',data,{})
+request.get = function (url, data) {
+    return request(url, 'get', data, {})
 }
 
 export default request
